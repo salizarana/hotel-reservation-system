db80eea2f21293359a2b9e32b70f446b1e4efb8b
salizarana <salizarana@gmail.com>
2024-01-21 21:45:46 +0545

    Initial setup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReservationComponent } from '../component/reservation/reservation.component';
+import { HeaderComponent } from '../component/header/header.component';
+import { FooterComponent } from '../component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import {HomeComponent} from '../component/home/home.component';
+import {AboutUsComponent} from '../component/about-us/about-us.component';
+import {ContactUsComponent} from '../component/contact-us/contact-us.component';
+import {ReservationComponent} from '../component/reservation/reservation.component';
+
+export const routes: Routes = [{path: '', redirectTo:'home', pathMatch:'full'}, {path: 'home', component: HomeComponent}, {path: 'reservation', component: ReservationComponent}, {path: 'about-us', component: AboutUsComponent},{path: 'contact-us', component: ContactUsComponent}];
