0c33fbc57b440b9bb6267a3b47ae22ba2529766f
salizarana <salizarana@gmail.com>
2024-01-23 22:38:12 +0545

    Add invoice.

--- src/component/invoice/invoice.component.ts
+++ src/component/invoice/invoice.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Invoice } from '../../interface/invoice';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './invoice.component.html',
-  styleUrl: './invoice.component.css'
+  styleUrl: './invoice.component.css',
 })
 export class InvoiceComponent {
-
+  constructor() {}
+  invoice: Invoice[] = [
+    {
+      date: '01/01/2024',
+      invoiceNumber: 10001,
+      fullName: 'Saliza Rana Magar',
+      phoneNumber: 9811111111,
+      email: 'saliza@gmail.com',
+      serialNumber: 1,
+      roomNumber: 101,
+      roomType: 'Deluxe Room',
+      arrivalDate: '01/02/2014',
+      departureDate: '01/03/2024',
+      rate: 1000,
+      amount: 1000,
+      subTotal: 1000,
+      discount: 0,
+      total: 1000,
+    },
+  ];
 }
