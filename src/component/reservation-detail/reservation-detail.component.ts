cf2feb20826f03d25ca88f87b364438c2b5c962c
salizarana <salizarana@gmail.com>
2024-01-24 20:46:29 +0545

    Handle form.

--- src/component/reservation-detail/reservation-detail.component.ts
+++ src/component/reservation-detail/reservation-detail.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-detail',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './reservation-detail.component.html',
-  styleUrl: './reservation-detail.component.css'
+  styleUrl: './reservation-detail.component.css',
 })
 export class ReservationDetailComponent {
-
+  onSubmit(getForm: NgForm) {
+    console.log(getForm.value);
+  }
 }
