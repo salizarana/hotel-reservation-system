582b1a10f3f12a0aad562058492b97025cc4c0b8
salizarana <salizarana@gmail.com>
2024-01-26 23:33:52 +0545

    Handle bookings.

--- src/component/bookings/bookings.component.ts
+++ src/component/bookings/bookings.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Room } from '../../interface/room';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bookings',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './bookings.component.html',
-  styleUrl: './bookings.component.css'
+  styleUrl: './bookings.component.css',
 })
 export class BookingsComponent {
-
+  @Input() rooms: Room[];
 }
